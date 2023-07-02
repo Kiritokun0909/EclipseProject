d86defd6c3bad7d2f720c7049f93b40874034460
LAPTOP-2PI3677V\hoduc <heolunkutu@gmail.com>
2023-07-02 12:15:18 +0700

    test 2134

--- EclipseProject/src/main/java/controller/HelloWorldController.java
+++ EclipseProject/src/main/java/controller/HelloWorldController.java
@@ -10,11 +10,8 @@ public class HelloWorldController {
 		return "index";
 	}
 	
-<<<<<<< HEAD
-	@RequestMapping("hello123987")
-=======
+
 	@RequestMapping("hello123456")
->>>>>>> origin/wibu
 	public String hello() {
 		return "hello";
 	}
